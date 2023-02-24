dee4b65bc6dccacc7f895ba83c7a0394ab456990
Mohamed KARIM <karimkar999@gmail.com>
2023-02-24 10:18:17 +0100

    done with check breackets

--- configfile/parse_conf_file.cpp
+++ configfile/parse_conf_file.cpp
@@ -6,20 +6,59 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 08:49:02 by mkarim            #+#    #+#             */
-/*   Updated: 2023/02/24 09:54:54 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/02/24 10:17:27 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "configfile.hpp"
 #include <stack>
 
-std::string		abstract_brackets(std::string str)
+void	exit_mode(std::string str)
 {
-	std::stack<char>	st;
-	std::string			brackets = "";
+	std::cout << "\033[31m" << "ERROR : " << str << std::endl;
 }
 
-void	check_brackets(std::string std)
+std::string		abstract_brackets(std::string str)
 {
-	std::string brackerts = abstract_brackets(std);
+	size_t				i;
+	std::string			brackets;
+
+	i = 0;
+	brackets = "";
+	while (str[i])
+	{
+		if (str[i] == '}' || str[i] == '{')
+			brackets += str[i];
+		i++;
+	}
+	return brackets;
+}
+
+void	valid_brackets(std::string str)
+{
+	std::stack<char>	st;
+	size_t				i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '{')
+			st.push(str[i]);
+		else if (str[i] == '}')
+		{
+			if (st.empty())
+				exit_mode("INVALID BRACKETS");
+			st.pop();
+		}
+		i++;
+	}
+	if (!st.empty())
+		exit_mode("INVALID BRACKETS");
+}
+
+void	check_brackets(std::string str)
+{
+	std::string brackets = abstract_brackets(str);
+	valid_brackets(brackets);
+	std::cout << "Brackets valid" << std::endl;
 }
