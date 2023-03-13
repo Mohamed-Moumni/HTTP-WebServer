9464bb7ea6b7f83ad22f1d1c178e8d6787dd5b36
yo <badaouiyoussef8888@gmail.com>
2023-03-13 14:34:31 +0100

    all the request parsed but need more tests

--- request/request.cpp
+++ request/request.cpp
@@ -43,10 +43,13 @@ int get_request_headers(request &request)
 
 int get_request_body(request &request)
 {
-    std::string request_body = request.request_string.substr(request.request_string.find("\r\n\r\n"));
+    std::string request_body = request.request_string.substr(request.request_string.find("\r\n\r\n") + 4);
 
-    if(request.request_string.size() && request.method != "POST")
-        return 0;
+    // if(request.request_string.size() && request.method != "POST")
+    //     return 0;
+    // else
+        request.request_body = request_body;
+    return 1;
     
 }
 
@@ -60,6 +63,11 @@ int pars_request(request &request)
     if(!get_request_headers(request))
     {
         std::cout << "unable to get requst headers" << std::endl;
+        return -1;
+    }
+    if(!get_request_body(request))
+    {
+        std::cout << "unable to get request body" << std::endl;
         return -1;
     }
     
@@ -76,7 +84,7 @@ int request_handler(request &request)
 int main()
 {
     request request;
-    request.request_string  = "GET / HTTP/1.1\r\nHost:localhost\nsec-ch-ua-mobile: ?0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\nConnection: close\r\n\r\n";
+    request.request_string  = "GET / HTTP/1.1\r\nHost:localhost\nsec-ch-ua-mobile: ?0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\nConnection: close\r\n\r\nhello everybody here is the body hahaha";
     // std::cout << request.request_string << std::endl;
     // request.request_string  = "1 2 3\r\n";
     request_handler(request);
@@ -89,6 +97,7 @@ int main()
     std::cout << request.headers_map["Host"] << std::endl;
     std::cout << request.headers_map["Connection"] << std::endl;
     std::cout << request.headers_map["User-Agent"] << std::endl;
+    std::cout << "+++" << request.request_body << "+++" << std::endl;
 
     
 }
