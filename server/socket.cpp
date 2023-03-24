4f3228e29c102fa4f2f015b985ed78dec86fda7a
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2023-03-24 15:34:47 +0000

    change: the error handling from check-return to try-catch

--- server/socket.cpp
+++ server/socket.cpp
@@ -6,7 +6,7 @@
 /*   By: mmoumni <mmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 18:23:49 by mmoumni           #+#    #+#             */
-/*   Updated: 2023/03/14 10:24:21 by mmoumni          ###   ########.fr       */
+/*   Updated: 2023/03/24 15:34:00 by mmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,7 @@ addrinfo    *Socket::getinfostruct(std::string hostname, std::string port)
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     if ((err = getaddrinfo(hostname.c_str(), port.c_str(), &hints, &res)) < 0)
-    {
-        std::cout << "getaddrinfo Error: " << gai_strerror(err) << std::endl;
-        exit(EXIT_FAILURE);
-    }
+        throw std::runtime_error("getaddrinfo Error: " + std::string(gai_strerror(err)));
     return (res);
 }
 
@@ -77,39 +74,17 @@ int Socket::createSocketId(addrinfo  *hints)
         if (sockId == -1)
             continue;
         if (fcntl(sockId, F_SETFL, O_NONBLOCK) < 0)
-        {
-            std::cout << "fcntl Error: " << std::endl;
-        }
+            throw std::runtime_error("fcntl Error: ");
         if ((setsockopt(sockId, SOL_SOCKET, SO_REUSEADDR, &var, sizeof(var))) < 0)
-        {
-            std::cout << "setsockopt Error: " << std::endl;
             continue;
-        }
         if ((bind(sockId, p->ai_addr, p->ai_addrlen)) < 0)
-        {
-            std::cout << strerror(errno) << std::endl;
-        }
+            throw std::runtime_error(std::string(strerror(errno)));
         else
             break ;
         close(sockId);
     }
     freeaddrinfo(hints);
     if (p == NULL)
-    {
-        std::cout << "Can't create the socket: invalid Ip address or port" << std::endl;
-        exit(EXIT_FAILURE);
-    }
+        throw std::runtime_error("Can't create the socket: invalid Ip address or port");
     return (sockId);
-}
-
-std::vector<Socket> CreateSockets(const std::vector<Server> & _server)
-{
-    size_t              ServerCount;
-    std::vector<Socket> sockets;
-
-    ServerCount = _server.size();
-    for (size_t i = 0; i < ServerCount; i++)
-    {
-                   
-    }
 }
