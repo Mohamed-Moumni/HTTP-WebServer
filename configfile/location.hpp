5ab674d5605615aae93e42fecafd6b5290c38b8d
Mohamed KARIM <karimkar999@gmail.com>
2023-04-02 14:11:08 +0000

    Add others attribute in location class
    Specify general attributes for general attributes except them sotre in others attriubte

--- configfile/location.hpp
+++ configfile/location.hpp
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:51:55 by mkarim            #+#    #+#             */
-/*   Updated: 2023/04/02 13:54:43 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/04/02 14:09:32 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,5 @@ class location
 		std::vector<std::string>							_allowed_methods;
 		std::map<std::string, std::string>					_error_pages;
 		std::map<std::string, std::string>					_return;
+		std::map<std::string, std::vector<std::string> >	_others;
 };
