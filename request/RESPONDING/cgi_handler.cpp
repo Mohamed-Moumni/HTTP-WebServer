a038e594c124d3a75db8f0f95daccdc43311a3ad
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2023-04-14 18:03:49 +0000

    cgi_handler

--- request/RESPONDING/cgi_handler.cpp
+++ request/RESPONDING/cgi_handler.cpp
@@ -1,5 +1,5 @@
 #include "../INCLUDES/request.hpp"
-
+#include <fcntl.h>
 
 static std::string  readFromPipe(int PipeId)
 {
@@ -20,17 +20,21 @@ void cgi_handler(ConnectSocket &socket, location location, ConfigFile configfile
     int         pid;
     int         fds[2];
     std::string body;
-    //setting env variables
-    //fork.....
-    set_env();
+    int         tmpfile;
+
     if (pipe(fds) < 0)
     {
         std::cout << "PIPE Error\n";
         return ;
     }
+    tmpfile = open("/tmp/tempfd", O_CREAT | O_RDWR , 0644);
+    write(tmpfile, socket._request.request_body.c_str(), socket._request.request_body.size());
+    lseek(tmpfile, 0, SEEK_SET);
     pid = fork();
     if (pid == 0)
     {
+        dup2(tmpfile, STDIN_FILENO);
+        close(tmpfile);
         dup2(fds[1], STDOUT_FILENO);
         close(fds[0]);
         close(fds[1]);
@@ -39,4 +43,5 @@ void cgi_handler(ConnectSocket &socket, location location, ConfigFile configfile
     body = readFromPipe(fds[0]);
     close(fds[0]);
     close(fds[1]);
+    unlink("/tmp/tempfd");
 }
