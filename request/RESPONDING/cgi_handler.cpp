5e298354e4c4babbe8e75cf7059b38349d03dde5
Youssef Badaoui <ybadaoui@e2r9p3.1337.ma>
2023-04-15 11:30:26 +0000

    cgi work with simple input

--- request/RESPONDING/cgi_handler.cpp
+++ request/RESPONDING/cgi_handler.cpp
@@ -6,16 +6,18 @@ std::string conc(std::string s1, std::string s2)
     return s1 + '=' + s2;
 }
 
-static std::string  readFromPipe(int PipeId)
+std::string  readFromPipe(int PipeId)
 {
     int             readedChar;
     std::string     data;
     char            buffer[BUFFER] = {0};
 
-    // readedChar = read(PipeId, buffer, BUFFER);
-    while ((readedChar = read(PipeId, buffer, BUFFER)) > 0)
+    int i = 0;
+    readedChar = read(PipeId, buffer, BUFFER);
+    while (readedChar > 0)
     {
-        data.append(buffer, readedChar);
+        data += buffer;
+        readedChar = read(PipeId, buffer, BUFFER);
     }
     return (data);
 }
@@ -23,31 +25,26 @@ static std::string  readFromPipe(int PipeId)
 void set_env(ConnectSocket &socket, location location,Server server, ConfigFile configfile, char **env)
 {
     std::string server_name = socket.IpAdress + ":" + socket.Port;
-    // if(socket._request.)
     std::string path_info = socket._request.original_request_target.substr(socket._request.original_request_target.find(".php") + 4,\
     (socket._request.original_request_target.find('?') ) - (socket._request.original_request_target.find(".php") + 4));
     std::string script_name = socket._request.request_target.substr(socket._request.request_target.find_last_of('/') + 1, socket._request.request_target.find(".php") + 4);
     std::string querystring;
     if(socket._request.original_request_target.find('?') != std::string::npos)
         querystring = socket._request.original_request_target.substr(socket._request.original_request_target.find('?') + 1);
-    std::cout << "server_name: "<< server_name << std::endl;
-    std::cout << "path_info: " << path_info << std::endl;
-    std::cout << "script_name: " << script_name << std::endl;
-    std::cout << "query_string: " << querystring << std::endl;
-
     env[0] = strdup("SERVER_SOFTWARE=NGINY/0.1");
     env[1] = strdup(conc("SERVER_NAME", server_name).c_str());
     env[2] = strdup(conc("SERVER_PROTOCOL", "HTTP/1.1").c_str());
     env[3] = strdup(conc("SERVER_PORT", socket.Port).c_str());
     env[4] = strdup(conc("REQUEST_METHOD", socket._request.method).c_str());
+    env[5] = strdup(conc("PATH_INFO", path_info).c_str());
+    env[6] = strdup(conc("SCRIPT_FILENAME", socket._request.request_target).c_str());
+    env[7] = strdup(conc("QUERY_STRING", querystring).c_str());
+    env[8] = strdup(conc("REDIRECT_STATUS", "301").c_str());
     if(socket._request.method == "POST")
     {
-        env[5] = strdup(conc("CONTENT_TYPE", socket._request.headers_map["Content-type"]).c_str());
-        env[6] = strdup(conc("CONTENT_LENGTH", socket._request.headers_map["Content-Length"]).c_str());
+        env[9] = strdup(conc("CONTENT_TYPE", socket._request.headers_map["Content-type"]).c_str());
+        env[10] = strdup(conc("CONTENT_LENGTH", socket._request.headers_map["Content-Length"]).c_str());
     }
-    env[7] = strdup(conc("PATH_INFO", path_info).c_str());
-    env[8] = strdup(conc("SCRIPT_NAME", script_name).c_str());
-    env[9] = strdup(conc("QUERY_STRING", querystring).c_str());
 }
 
 void cgi_handler(ConnectSocket &socket, location location,Server server, ConfigFile configfile)
@@ -68,32 +65,30 @@ void cgi_handler(ConnectSocket &socket, location location,Server server, ConfigF
     pid = fork();
     if (pid == 0)
     {
-        char *env[10];
-        char *args[2];
+        char *env[12];
+        char *args[3];
 
         set_env(socket, location, server, configfile, env);
-        args[0] = strdup("/bin/echo");
+        if(socket._request.method == "POST")
+            env[11] = NULL;
+        else
+            env[9] = NULL;
+        args[0] = strdup("./cgi-bin/php-cgi");
         args[1] = strdup(socket._request.request_target.c_str());
-        // dup2(tmpfile, STDIN_FILENO);
-        // std::cout << "path to script : " << args[1] << std::endl;
-        // close(tmpfile);
-        // dup2(fds[1], STDOUT_FILENO);
-        // close(fds[0]);
-        // close(fds[1]);
-        // std::ofstream file;
-        // file.open("cgi-bin/php-cgi");
-        std::cout << "before execve" << std::endl;
-        if(execve(args[0],args, NULL) == -1)
+        args[2] = NULL;
+        dup2(tmpfile, STDIN_FILENO);
+        close(tmpfile);
+        dup2(fds[1], STDOUT_FILENO);
+        close(fds[0]);
+        close(fds[1]);
+        if(execve(args[0], args, env) == -1)
             std::cout << errno << std::endl;
     }
+    
     waitpid(pid, NULL, 0);
-    // read data written from the pipe
-    // char buffer[30];
-    // read(fds[0], buffer, 30);
-    // std::cout << buffer << std::endl;
-    // body = readFromPipe(fds[0]);
-    // std::cout << "body : "<< body << std::endl;
-    // close(fds[0]);
-    // close(fds[1]);
+    close(fds[1]);
+    socket._response.response_string = readFromPipe(fds[0]);
+    std::cout << "body: +++++++++++++++++\n" << body << "+++++++++++++++++++++\n";
+    close(fds[0]);
     unlink("/tmp/tempfd");
 }
--- request/server_root/php/add.cgi
+++ request/server_root/php/add.cgi
@@ -4,10 +4,10 @@
 // Retrieve the query parameters from the URL
 $queryString = getenv('QUERY_STRING');
 
-// // Parse the query string to extract the two numbers
+// Parse the query string to extract the two numbers
 parse_str($queryString, $params);
 
-// // Get the values of the two numbers from the query parameters
+// Get the values of the two numbers from the query parameters
 $num1 = $params['num1'] ?? 0; // Default to 0 if not provided
 $num2 = $params['num2'] ?? 0; // Default to 0 if not provided
 
@@ -18,5 +18,6 @@ $sum = $num1 + $num2;
 header('Content-Type: text/plain');
 
 // Print the sum of the two numbers
-echo "The sum of $num1 and $num2 is: $sum\n";
+echo "The sum of $num1 and $num2 is: $sum";
+
 ?>
