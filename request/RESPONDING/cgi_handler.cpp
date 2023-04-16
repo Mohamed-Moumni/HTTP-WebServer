fe86da1121767f0a7b8f6660f294046530814a09
Youssef Badaoui <ybadaoui@e2r7p2.1337.ma>
2023-04-16 11:04:24 +0000

    trying to hundel multi cgi

--- request/RESPONDING/cgi_handler.cpp
+++ request/RESPONDING/cgi_handler.cpp
@@ -39,30 +39,53 @@ static std::string  readFromPipe(int PipeId)
     return (data);
 }
 
+// void set_env(ConnectSocket &socket, location location,Server server, ConfigFile configfile, char **env)
+// {
+//     //setting SERVER_NAME
+//     std::string server_name = socket.IpAdress + ":" + socket.Port;
+
+//     //setting PATH_INFO
+//     std::string path_info = socket._request.original_request_target.substr(socket._request.original_request_target.find(location._cgiExt) + location._cgiExt.size(),\
+//     (socket._request.original_request_target.find('?') ) - (socket._request.original_request_target.find(location._cgiExt) + location._cgiExt.size()));
+
+//     //setting SCRIPTNAME
+//     std::string script_name = socket._request.request_target.substr(socket._request.request_target.find_last_of('/')\
+//     + 1, socket._request.request_target.find(location._cgiExt) + location._cgiExt.size());
+
+//     //setting QUERYSTRING
+//     std::string querystring;
+//     if(socket._request.method == "GET")
+//     {
+//         if(socket._request.original_request_target.find('?') != std::string::npos)
+//             querystring = socket._request.original_request_target.substr(socket._request.original_request_target.find('?') + 1);
+//     }
+//     // else
+//     //     querystring = socket._request.request_body;
+
+//     env[0] = strdup("SERVER_SOFTWARE=NGINY/0.1");
+//     env[1] = strdup(conc("SERVER_NAME", server_name).c_str());
+//     env[2] = strdup(conc("SERVER_PROTOCOL", "HTTP/1.1").c_str());
+//     env[3] = strdup(conc("SERVER_PORT", socket.Port).c_str());
+//     env[4] = strdup(conc("REQUEST_METHOD", socket._request.method).c_str());
+//     env[5] = strdup(conc("PATH_INFO", path_info).c_str());
+//     env[6] = strdup(conc("SCRIPT_FILENAME", socket._request.request_target).c_str());
+//     env[7] = strdup(conc("QUERY_STRING", querystring).c_str());
+//     env[8] = strdup(conc("REDIRECT_STATUS", "301").c_str());
+//     if(socket._request.method == "POST")
+//     {
+//         env[9] = strdup(conc("CONTENT_TYPE", socket._request.headers_map["Content-Type"]).c_str());
+//         env[10] = strdup(conc("CONTENT_LENGTH", socket._request.headers_map["Content-Length"]).c_str());
+//     }
+// }
 void set_env(ConnectSocket &socket, location location,Server server, ConfigFile configfile, char **env)
 {
-    //setting SERVER_NAME
     std::string server_name = socket.IpAdress + ":" + socket.Port;
-
-    //setting PATH_INFO
-    std::string path_info = socket._request.original_request_target.substr(socket._request.original_request_target.find(location._cgiExt) + location._cgiExt.size(),\
-    (socket._request.original_request_target.find('?') ) - (socket._request.original_request_target.find(location._cgiExt) + location._cgiExt.size()));
-
-    // std::cout << "ext: " << location._cgiExt << std::endl;
-    //setting SCRIPTNAME
-    std::string script_name = socket._request.request_target.substr(socket._request.request_target.find_last_of('/')\
-    + 1, socket._request.request_target.find(location._cgiExt) + location._cgiExt.size());
-
-    //setting QUERYSTRING
+    std::string path_info = socket._request.original_request_target.substr(socket._request.original_request_target.find(".php") + 4,\
+    (socket._request.original_request_target.find('?') ) - (socket._request.original_request_target.find(".php") + 4));
+    std::string script_name = socket._request.request_target.substr(socket._request.request_target.find_last_of('/') + 1, socket._request.request_target.find(".php") + 4);
     std::string querystring;
-    if(socket._request.method == "GET")
-    {
-        if(socket._request.original_request_target.find('?') != std::string::npos)
-            querystring = socket._request.original_request_target.substr(socket._request.original_request_target.find('?') + 1);
-    }
-    else
-        querystring = socket._request.request_body;
-
+    if(socket._request.original_request_target.find('?') != std::string::npos)
+        querystring = socket._request.original_request_target.substr(socket._request.original_request_target.find('?') + 1);
     env[0] = strdup("SERVER_SOFTWARE=NGINY/0.1");
     env[1] = strdup(conc("SERVER_NAME", server_name).c_str());
     env[2] = strdup(conc("SERVER_PROTOCOL", "HTTP/1.1").c_str());
@@ -70,12 +93,11 @@ void set_env(ConnectSocket &socket, location location,Server server, ConfigFile 
     env[4] = strdup(conc("REQUEST_METHOD", socket._request.method).c_str());
     env[5] = strdup(conc("PATH_INFO", path_info).c_str());
     env[6] = strdup(conc("SCRIPT_FILENAME", socket._request.request_target).c_str());
-    std::cout << "script_file_name: " << env[6] << std::endl;
     env[7] = strdup(conc("QUERY_STRING", querystring).c_str());
     env[8] = strdup(conc("REDIRECT_STATUS", "301").c_str());
     if(socket._request.method == "POST")
     {
-        env[9] = strdup(conc("CONTENT_TYPE", socket._request.headers_map["Content-Type"]).c_str());
+        env[9] = strdup(conc("CONTENT_TYPE", socket._request.headers_map["Content-type"]).c_str());
         env[10] = strdup(conc("CONTENT_LENGTH", socket._request.headers_map["Content-Length"]).c_str());
     }
 }
@@ -108,23 +130,21 @@ void cgi_handler(ConnectSocket &socket, location location,Server server, ConfigF
             env[11] = NULL;
         else
             env[9] = NULL;
-        // args[0] = strdup(location._cgiPath.c_str());
-        args[0] = strdup("./php-cgi");
-        std::cout << args[0] << std::endl;
+
+        args[0] = strdup(('.' + location._cgiPath).c_str());
         args[1] = strdup(socket._request.request_target.c_str());
         args[2] = NULL;
-        // dup2(tmpfile, STDIN_FILENO);
-        // close(tmpfile);
-        // dup2(fds[1], STDOUT_FILENO);
-        // close(fds[0]);
-        // close(fds[1]);
-        for(int i = 0; i < 12; i++)
-            std::cout << env[i] << std::endl;
-        std::cout << "before execve\n";
+        dup2(tmpfile, STDIN_FILENO);
+        close(tmpfile);
+        dup2(fds[1], STDOUT_FILENO);
+        close(fds[0]);
+        close(fds[1]);
         if(execve(args[0], args, env) == -1)
-            std::cout << errno << std::endl;
-        std::cout << "after execve\n";
+        {
+            std::cout << "execve: "<< errno << std::endl;
+        }
     }
+
     long long rest = getTimeOfNow() - timeOut;
     while (rest < 5)
     {
@@ -132,7 +152,7 @@ void cgi_handler(ConnectSocket &socket, location location,Server server, ConfigF
         {
             close(fds[1]);
             socket._response.response_string = readFromPipe(fds[0]);
-            // std::cout << "body: +++++++++++++++++\n" << body << "+++++++++++++++++++++\n";
+            std::cout << "body: +++++++++++++++++\n" << body << "+++++++++++++++++++++\n";
             close(fds[0]);
             std::ostringstream out;
             out << "HTTP/1.1 200 OK\r\nSet-Cookie: NginY=" << generateToken(10) << ";Max-Age=30"<< "\r\nContent-Length: " << socket._response.response_string.substr(socket._response.response_string.find("\r\n\r\n") + 4).size() << "\r\n" << socket._response.response_string;
@@ -144,7 +164,7 @@ void cgi_handler(ConnectSocket &socket, location location,Server server, ConfigF
         else
             rest = getTimeOfNow() - timeOut;
     }
-    // kill(pid, SIGKILL);
+    kill(pid, SIGKILL);
     close(fds[1]);
     close(fds[0]);
     unlink("/tmp/tempfd");
